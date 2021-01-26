6be0aefd027de8dae346071b6d7cc8c218a71fe5
Clément Le Biez <clement@marmelab.com>
2021-01-26 11:19:52 +0100

    Using BOARD_SIZE constant to detect and draw quarters

--- src/render.py
+++ src/render.py
@@ -1,4 +1,5 @@
 import os
+from constant import BOARD_SIZE
 
 def get_marble_character(value):
     return "◯" if value == 0 else "◉"
@@ -8,15 +9,18 @@ def print_board(board, clear_before_printing = True):
     if clear_before_printing:
         os.system("clear")
 
+    # Used to detect when adding a line for drawing quarters.
+    HALF_BOARD_SIZE = (BOARD_SIZE / 2)
+
     print("\n      A  B  C   D  E  F")
     print("   ┌──────────+─────────┐")
     for x, line in enumerate(board, 0):
-        if (x + 1) % 4 == 0:
+        if x != 0 and x % HALF_BOARD_SIZE == 0:
             print("   |──────────+─────────|")
         line_values = ""
 
         for y, value in enumerate(line, 0):
-            if (y + 1) % 4 == 0:
+            if y != 0 and y % HALF_BOARD_SIZE == 0:
                 line_values = line_values + "|"
             line_values = line_values + " " + get_marble_character(value)
             
