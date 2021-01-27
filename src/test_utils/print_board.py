f30ca747c5c14a4bc9ce8a5866783fbdc517884a
Clément Le Biez <clement@marmelab.com>
2021-01-27 11:10:06 +0100

    Fix rebase

--- src/test_utils/print_board.py
+++ src/test_utils/print_board.py
@@ -1,6 +1,7 @@
 import sys
 from render import print_board
+from constant import PRINT_BOARD_PLACE_MARBLE
 
 def print_board_if_verbosity_is_set(board):
     if "-v" in sys.argv:
-        print_board(board, False)
+        print_board(board, PRINT_BOARD_PLACE_MARBLE, False)
